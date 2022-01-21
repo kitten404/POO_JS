7dedd24b42df187b629270f48d633c11391b1f5b
kitten404 <quirinoquezia@gmail.com>
2022-01-20 23:32:38 -0300

    criacao do cliente

---
+++ index.js
@@ -0,0 +1,14 @@
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+const cliente1 = new Cliente();
+cliente1.nome ="Quezia";
+cliente1.cpf = 45478565862;
+cliente1.agencia = 2002;
+cliente1.saldo = 99.25;
+
+console.log(cliente1);
